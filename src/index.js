d24d504972d9e0e762ec8eb20333c8fb8e29920a
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-05-23 20:36:24 +0300

    Create array of data to displayed

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+
+const data = [
+  {
+    name: 'Habtamu',
+    score: 100,
+  },
+  {
+    name: 'Alemayeh',
+    score: 200,
+  },
+  {
+    name: 'Bena',
+    score: 50,
+  },
+  {
+    name: 'Habtamu Alemayehu',
+    score: 50,
+  },
+  {
+    name: 'Microverse',
+    score: 5010,
+  },
+  {
+    name: 'Alexander',
+    score: 50,
+  },
+];
+
