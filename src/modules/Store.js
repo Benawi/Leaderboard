06aafe8cbfe7e6524906363ec7e90d6bf64a3182
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-05-24 16:50:31 +0300

    Create store class with constractor and init URL

---
+++ src/modules/Store.js
@@ -0,0 +1,8 @@
+export default class Store {
+    constructor() {
+      this.data = [];
+      this.scoresUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/266Uox8RtUVX6YlFdld7/scores/';
+    }
+  
+    
+  }
