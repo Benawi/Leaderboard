adf5bdf68466a2d295188937b2d907d52f5b9490
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-05-24 16:54:19 +0300

    Define addscrore method

--- src/modules/Store.js
+++ src/modules/Store.js
@@ -4,5 +4,11 @@ export default class Store {
       this.scoresUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/266Uox8RtUVX6YlFdld7/scores/';
     }
   
-    
+    addNewItem(user, score) {
+      this.obj = {
+        user,
+        score,
+      };
+  
+     
   }
